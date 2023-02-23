263c2d5e8d183eeec3a519cc69a03ad12349b07f
YOU PAN <sunnypanyou@gmail.com>
2023-02-23 11:03:16 -0500

    not finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+4.times do
+  Flat.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    description: Faker::Markdown.emphasis,
+    price_per_night: rand(50..100),
+    number_of_guests: rand(1..6)
+  )
+end
